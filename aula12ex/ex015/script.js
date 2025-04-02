9d3cd67515f93cb89e00bf1d1457e2b90ab5460e
Matheus Jesus <MatheusJesusJesus07@hotmail.com>
2025-04-02 16:57:30 -0300

    finalizei a tela de ano de nascimento

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -6,8 +6,40 @@ function verificar() {
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementById('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - (fano.value)
-        res.innerHTML = `Idade calculada: ${idade} anos.`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        
+        if (fsex[0].checked) {
+            gênero = 'homem'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'mulher'
+            if (idade >=0 && idade < 10) {
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulta-f.png')
+            } else {
+                img.setAttribute('src', 'foto-idosa-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos um(a) <strong>${gênero}</strong> com ${idade} anos.`
+        res.appendChild(img)
+        res.appendChild(img).style.width = '240px'
+        res.appendChild(img).style.padding = '15px'
     }
+    
 }
