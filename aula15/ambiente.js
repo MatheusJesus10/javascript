9b44dcaf67dd418b10f49d28d6cb1eff6e181805
Matheus Jesus <MatheusJesusJesus07@hotmail.com>
2025-04-03 16:53:17 -0300

    criei a aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 9, 3, 2]
+
+num.sort() //colocar o vetor em ordem
+num.push(1) //coloca o número 1 no final do array
+console.log(num)
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
