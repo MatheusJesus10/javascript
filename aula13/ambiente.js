a93a9ebf5d057d07a0c863a68ce1d2dfa7c45e89
Matheus Jesus <MatheusJesusJesus07@hotmail.com>
2025-04-03 09:21:47 -0300

    exercício aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`teste ${c}`)
+    c++
+} while (c <= 10)
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`teste ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('teste')
+console.log('teste')
+console.log('teste')
+console.log('teste')
+console.log('teste')
+console.log('teste')
+*/
