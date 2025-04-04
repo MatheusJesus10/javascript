cc598be5a827f76902f92d499c2c51ba6d0527bc
Matheus Jesus <MatheusJesusJesus07@hotmail.com>
2025-04-04 09:41:21 -0300

    criei aula 15

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,7 +1,14 @@
 let num = [5, 8, 9, 3, 2]
 
-num.sort() //colocar o vetor em ordem
 num.push(1) //coloca o número 1 no final do array
+num.sort() //colocar o vetor em ordem
 console.log(num)
 console.log(`O vetor tem ${num.length} posições.`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(5)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}.`)
+}
